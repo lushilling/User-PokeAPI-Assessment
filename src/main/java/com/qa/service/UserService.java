97071e5c48565c3ac71e9b89a177c9a2ce8af690
lushilling <lshilling@hotmail.co.uk>
2019-08-09 09:42:59 +0100

    service layer

--- src/main/java/com/qa/service/UserService.java
+++ src/main/java/com/qa/service/UserService.java
@@ -1,5 +1,17 @@
 package com.qa.service;
 
+import java.util.Collection;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.persistance.entity.User;
+
+@Service
 public interface UserService {
+	
+	Collection<User> getAllUsers();
+	User getUser(Long id);
+	String deleteUser(User user);
+	User addUser(User User);
 
 }
--- src/main/java/com/qa/service/UserServiceImplemented.java
+++ src/main/java/com/qa/service/UserServiceImplemented.java
@@ -1,5 +1,33 @@
 package com.qa.service;
 
+import java.util.Collection;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.persistance.entity.User;
+import com.qa.persistance.repository.UserDatabaseRepository;
+
+@Service
 public class UserServiceImplemented implements UserService {
+	
+	private UserDatabaseRepository userRepo;
+
+	public Collection<User> getAllUsers() {
+		return userRepo.findAll();
+	}
+
+	public User getUser(Long id) {
+		User user = userRepo.findById(id).get();
+		return user;
+	}
+
+	public String deleteUser(User user) {
+		userRepo.delete(user);
+		return "Account Deleted";
+	}
+
+	public User addUser(User user) {
+		return userRepo.save(user);
+	}
 
 }
