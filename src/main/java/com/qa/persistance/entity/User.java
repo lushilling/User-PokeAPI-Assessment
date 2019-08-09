ebc03caafdbb8485223c11eefffde4c5224498f9
lushilling <lshilling@hotmail.co.uk>
2019-08-09 09:34:43 +0100

    user entity made

--- src/main/java/com/qa/persistance/entity/User.java
+++ src/main/java/com/qa/persistance/entity/User.java
@@ -1,5 +1,39 @@
 package com.qa.persistance.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Long memberNumber;
+	
+	public User(Long id, String name, Long memberNumber) {
+		super();
+		this.id= id;
+		this.name = name;
+		this.memberNumber = memberNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getMemberNumber() {
+		return memberNumber;
+	}
+
+	public void setMemberNumber(Long memberNumber) {
+		this.memberNumber = memberNumber;
+	}
 
 }
